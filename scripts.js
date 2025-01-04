b16c6d00976ffd4ab884126d9c515d91a673b223
Alex Roper <alex@esrl.org>
2025-01-04 16:11:24 -0500

    Created getComputerChoice and getUserChoice functions.

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        return 'rock';
+    } else if (computerChoice === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+function getUserChoice() {
+    return prompt('Choose rock, paper, or scissors.');
+}
