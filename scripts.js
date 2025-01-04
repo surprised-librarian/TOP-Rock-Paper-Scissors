9d33dc969f5996cf0fc129ad0486856538cd8a52
Alex Roper <alex@esrl.org>
2025-01-04 16:26:39 -0500

    Added playRound function with logic to play 1 round.

--- scripts.js
+++ scripts.js
@@ -22,3 +22,33 @@ function getUserChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+//Function to play 1 round
+
+function playRound() {
+    let computer = getComputerChoice();
+    let user = getUserChoice();
+    console.log("Computer chooses " + computer);
+    console.log("Computer chooses " + user);
+    if (computer === user){
+        console.log("It's a tie!");
+    }   else if (user == 'rock') {
+        if (computer == 'paper') {
+            console.log("Computer wins!");
+        } else {
+            console.log("You win!");
+        }
+    }   else if (user == 'paper') {
+        if (computer == 'rock') {
+            console.log("You win!");
+        } else {
+            console.log("Computer wins");
+        }
+    }   else {
+        if (computer == 'rock') {
+            console.log("Computer wins!");
+        } else {
+            console.log("You Win!");
+        }
+    }
+
+}
